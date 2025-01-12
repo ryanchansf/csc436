accd1149d9940cdd98bfa93bb02f4d1d19ca9e08
Ryan Chan <ryanchansf@gmail.com>
2025-01-12 14:18:57 -0800

    add grad cap as image composable

--- lab1/GraduationRSVP/app/src/main/java/com/example/graduationrsvp/MainActivity.kt
+++ lab1/GraduationRSVP/app/src/main/java/com/example/graduationrsvp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.zybooks.graduationrsvp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -37,6 +39,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GradScreen(modifier: Modifier = Modifier) {
+    Image(
+        painter= painterResource(R.drawable.grad_cap),
+        contentDescription = stringResource(R.string.graduation_cap),
+        alpha = 0.3f
+    )
     Text(
         text = stringResource(R.string.graduation_announcement),
         fontSize = 50.sp,
